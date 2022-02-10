ec253c314853adb0ec5d02922527c6853246cc26
queicherius <queicherius@gmail.com>
2022-02-10 16:39:55 +0000

    Set headers

--- src/gw2wiki/helpers.js
+++ src/gw2wiki/helpers.js
@@ -3,10 +3,14 @@ import querystring from 'querystringify'
 
 export default { getWikiHtml, getWikiMarkup, getWikiImage }
 
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
+}
+
 // Get the wiki html for the given title
 export async function getWikiHtml (title) {
   let url = 'http://wiki.guildwars2.com/wiki/' + encodeURIComponent(title)
-  return fetch.single(url, { type: 'text' })
+  return fetch.single(url, { headers, type: 'text' })
 }
 
 // Get the wiki markup for the given title
@@ -20,7 +24,7 @@ export async function getWikiMarkup (title) {
   }
   let url = 'http://wiki.guildwars2.com/api.php?' + querystring.stringify(query)
 
-  let content = await fetch.single(url)
+  let content = await fetch.single(url, { headers })
   let pageId = Object.keys(content.query.pages)[0]
 
   return content.query.pages[pageId].revisions[0]['*']
@@ -37,7 +41,7 @@ export async function getWikiImage (file) {
   }
   let url = 'http://wiki.guildwars2.com/api.php?' + querystring.stringify(query)
 
-  let content = await fetch.single(url)
+  let content = await fetch.single(url, { headers })
   let pageId = Object.keys(content.query.pages)[0]
   let imageinfo = content.query.pages[pageId].imageinfo
 
