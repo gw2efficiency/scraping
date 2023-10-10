55ce13f5f94ef5012a3099236c17ab3618e434f0
darthmaim <git@darthmaim.de>
2023-10-10 23:26:44 +0200

    Remove nested query

--- src/gw2wiki/blackLionStatuette.js
+++ src/gw2wiki/blackLionStatuette.js
@@ -2,10 +2,10 @@ import { getSmwQueryResult } from './helpers.js'
 
 // Get the current claim ticket offers
 export default async function blackLionStatuette () {
-  const result = await getSmwQueryResult('[[Has vendor::Black Lion Statuette]][[Has item cost.Has item currency::Black Lion Statuette]]|?Sells item=item|?Has item cost.Has item value=value')
+  const result = await getSmwQueryResult('[[Has vendor::Black Lion Statuette]][[Has item cost.Has item currency::Black Lion Statuette]]|?Sells item=item|?Has item cost=value')
 
   const map = Object.values(result.query.results).map(data => {
-    return [data.printouts['item'][0].fulltext, parseInt(data.printouts['value'][0])]
+    return [data.printouts['item'][0].fulltext, parseInt(data.printouts['value'][0]['Has item value'].item[0])]
   })
 
   return Object.fromEntries(map)
